1580888732e2269d73b2114cf8b9a03289d3102d
Katharine Chui <kwchuiaa@connect.ust.hk>
2023-08-18 01:38:00 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,34 @@
+sudo bash -c '
+set -ex
+
+rm -rf image_out
+
+BUILD_TARGETS="with-KDE with-GNOME"
+
+for profile in $BUILD_TARGETS
+do
+
+	while umount tmp_mnt
+	do
+		echo unmount tmp_mnt
+	done
+
+	rm -rf tmp_mnt
+	mkdir -p tmp_mnt
+
+	mount -t tmpfs -o size=10G, tmpfs tmp_mnt
+
+
+	if ! kiwi-ng --profile=$profile system prepare --description=./ --root=./tmp_mnt/kiwi_root_dir
+	then
+		echo prepare failed, copying out root_dir for inspection
+		rm -rf root_dir_debug
+		tar -C ./tmp_mnt -cO kiwi_root_dir | tar -x
+		mv kiwi_root_dir root_dir_debug
+		exit 1
+	fi
+
+	kiwi-ng --profile=$profile system create --root=./tmp_mnt/kiwi_root_dir --target-dir=./image_out/$profile/
+	umount tmp_mnt
+done
+'
---
+++ config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -xe
+
+# install input methods and some fonts for fallback
+set +e
+zypper -n --gpg-auto-import-keys install 'google-noto-*' 'noto-*-fonts' 'ibus-*'
+status_code=$?
+if [ "$status_code" != "107" ] && [ "$status_code" != "0" ]
+then
+	echo zypper -n --gpg-auto-import-keys install 'google-noto-*' 'noto-*-fonts' 'ibus-*' exited with $status_code
+	exit 1
+fi
+set -e
+
+# manpages
+zypper -n --gpg-auto-import-keys install man man-pages
+
+zypper -n clean -a
+
+# disable root
+passwd -l root
+# steamos update dummys sudoer file is 99
+echo '%wheel  ALL=(ALL)       ALL' > /etc/sudoers.d/98_deck_pw_sudo
+sed -iE '/^ALL   ALL=(ALL) ALL/d' /etc/sudoers
+sed -iE '/^Defaults targetpw/d' /etc/sudoers
+
+# configure snapper as https://build.opensuse.org/package/show/openSUSE:Factory/openSUSE-MicroOS would
+cp /usr/share/snapper/config-templates/default /etc/snapper/configs/root
+
+sed -i'' 's/^TIMELINE_CREATE=.*$/TIMELINE_CREATE="no"/g' /etc/snapper/configs/root
+sed -i'' 's/^NUMBER_LIMIT=.*$/NUMBER_LIMIT="2-10"/g' /etc/snapper/configs/root
+sed -i'' 's/^NUMBER_LIMIT_IMPORTANT=.*$/NUMBER_LIMIT_IMPORTANT="4-10"/g' /etc/snapper/configs/root
+
+# toggle some services
+systemctl enable NetworkManager
+systemctl disable sshd
+systemctl disable firewalld
+
+# force ibus
+echo 'export GTK_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT_IM_MODULE=ibus
+export EDITOR=nano' >> /etc/skel/.profile
+
+cp /etc/skel/.profile /home/katharine/.profile
+chown katharine:katharine /home/katharine/.profile
